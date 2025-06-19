2157628ef9b184be7d1f1046910d84a71af97b2a
Noor Alisha S <147066651+NoorAlisha22@users.noreply.github.com>
2025-06-19 20:57:44 +0530

    Update Login.py

--- Backend/Login.py
+++ Backend/Login.py
@@ -17,10 +17,10 @@ app.add_middleware(
 
 # Database connection
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="info",
+    host="sql12.freesqldatabase.com",
+    user="sql12785742",
+    password="xZrFFriYiV",
+    database="sql12785742",
     port=3306
 )
 
