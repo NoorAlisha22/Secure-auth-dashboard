c41b4153ee9d729eec77b1f2c374115457f07dd7
Noor Alisha S <147066651+NoorAlisha22@users.noreply.github.com>
2025-06-19 22:33:32 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -7,10 +7,13 @@ from UserInfo import retrieve_data
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Secure Auth Dashboard API is running!"}
+
+
 app.add_api_route("/Validate", ValidateFunc, methods=["POST"])
 app.add_api_route("/AuthTable", func, methods=["GET"])
 app.add_api_route("/insert", insert, methods=["POST"])
 app.add_api_route("/reset-password", frgtpass, methods=["POST"])
 app.add_api_route("/select", retrieve_data, methods=["POST"])
-
-
